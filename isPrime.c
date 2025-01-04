ff284fd0f66b5682147e690a443f165f1fa776ee
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2025-01-04 07:24:33 +0530

    prime number or not
    if a number is divisible by its own number and 1 then its a prime..

---
+++ isPrime.c
@@ -0,0 +1,28 @@
+// leap year or not 
+#include<stdio.h>
+int leap(int p)
+{
+    int sum=0;
+    for(int i =1;i<=p;i++)
+    {
+        if(p%i==0)
+        {
+            sum +=1;
+        }
+         
+    }
+    if(sum == 2)
+    {
+        printf("prime");
+    }
+    else
+    {
+        printf("not a prime");
+    }
+}
+void main()
+{
+    int num;
+    scanf("%d",&num);
+    leap(num);
+}
