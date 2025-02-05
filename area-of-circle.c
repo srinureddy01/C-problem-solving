247f256bed9e2c26249b59dd430bc599c8bf13ae
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2025-02-05 10:20:46 +0530

    Area of a circle
    here the direct equation  was 
    area = pi*r*r

---
+++ area-of-circle.c
@@ -0,0 +1,11 @@
+ // area of the circe
+ #include<stdio.h>
+ void main()
+ {
+     float r , area , pi=3.14;
+     printf("Enter the radious of a circle: ");
+     scanf("%f",&r);
+     
+     area = pi*r*r;
+     printf("The area of the circle was: %2f",area);
+ }
